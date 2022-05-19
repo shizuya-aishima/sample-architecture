281f89baee297c73eebe323c7931f6e44908311f
shizuya-aishima <szy@live.jp>
2022-05-19 11:08:07 +0000

    :+1: update: gradle build が動いたためコミット

--- item/src/test/java/com/example/item/ItemApplicationTests.java
+++ item/src/test/java/com/example/item/ItemApplicationTests.java
@@ -1,10 +1,27 @@
 package com.example.item;
 
+import com.google.api.gax.core.FixedCredentialsProvider;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.FirestoreOptions;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ItemApplicationTests {
+
+	@BeforeAll
+	public static void befor() {
+		Firestore FS = FirestoreOptions.getDefaultInstance().toBuilder()
+						.setProjectId("sample")
+						.setHost("localhost:8888")
+						.setCredentials(new FirestoreOptions.EmulatorCredentials())
+						.setCredentialsProvider(FixedCredentialsProvider.create(new FirestoreOptions.EmulatorCredentials()))
+						.build()
+						.getService();
+		System.out.println(FS.document("doc/test"));
+	}
 
 	@Test
 	void contextLoads() {
