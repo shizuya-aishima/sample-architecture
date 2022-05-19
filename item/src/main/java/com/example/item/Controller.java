8e955475b0de0bb990f16aeb1ef15dac070612d8
shizuya-aishima <szy@live.jp>
2022-05-19 11:29:52 +0000

    :+1: update: 仮作成

--- item/src/main/java/com/example/item/Controller.java
+++ item/src/main/java/com/example/item/Controller.java
@@ -3,17 +3,20 @@ package com.example.item;
 import com.example.grpc.item.ItemGrpc.ItemImplBase;
 import com.example.grpc.item.ItemOuterClass.CreateReply;
 import com.example.grpc.item.ItemOuterClass.CreateRequest;
-
+import com.example.grpc.item.ItemOuterClass.Status;
 import org.lognet.springboot.grpc.GRpcService;
-
 import io.grpc.stub.StreamObserver;
 
 @GRpcService
 public class Controller extends ItemImplBase {
+
   @Override
   public void create(CreateRequest request, StreamObserver<CreateReply> responseObserver) {
     // TODO Auto-generated method stub
     super.create(request, responseObserver);
-
+    var reply = CreateReply.newBuilder().setStatus(Status.PENDING).build();
+    responseObserver.onNext(reply);
+    responseObserver.onCompleted();
   }
+
 }
